28277e825c186ccdc2a8c013ee61bb3ffe9ff8e3
Sithum Witharana <witharana92@gmail.com>
2023-01-11 17:40:42 +0530

    Add project files.

---
+++ CodoERP/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodoERP.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodoERP.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodoERP.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
